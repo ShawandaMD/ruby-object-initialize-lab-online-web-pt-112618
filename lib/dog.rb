ab1d7e6924bf4a2f62e4d88f82075f97505c953b
Shawanda Davis <shawandadavis93@gmail.com>
2018-12-27 15:28:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
-require pry
+require 'pry'
 class Dog 
   def initialize(name, breed)
     @name = name
     @breed = breed
     if @breed == ""
       @breed
+    binding.pry  
     end  
   end
 end  
